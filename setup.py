e305e1baeee53e2de328b9601e954e5124cf62b5
matt-behrens-1 <95661505+matt-behrens-1@users.noreply.github.com>
2021-12-28 14:51:49 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         "pycryptodome~=3.11.0",
         "python-dotenv~=0.19.1",
         "pytz~=2021.3",
-        "confuse~=1.6.0",
+        "confuse~=1.4.0",
     ],
     packages=['ad_api','ad_api.api','ad_api.auth','ad_api.base','ad_api.api.sp','ad_api.api.sb','ad_api.api.sd'],
     url='https://github.com/denisneuf/python-amazon-ad-api',
