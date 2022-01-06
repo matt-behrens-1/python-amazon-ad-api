dff9758a7492a62078f396e97e342e6f3406b940
matt-behrens-1 <95661505+matt-behrens-1@users.noreply.github.com>
2022-01-06 16:01:23 -0700

    Update __init__.py

--- ad_api/api/sb/__init__.py
+++ ad_api/api/sb/__init__.py
@@ -15,6 +15,7 @@ from .bid_recommendations import BidRecommendations
 from .stores import Stores
 from .media import Media
 from .reports import Reports
+from .snapshots import Snapshots
 
 __all__ = [
     "Brands",
@@ -31,7 +32,8 @@ __all__ = [
     "NegativeTargets",
     "TargetsRecommendations",
     "BidRecommendations",
-    "Stores",
+    "Stores,
     "Media",
-    "Reports"
+    "Reports",
+    "Snapshots"
 ]
